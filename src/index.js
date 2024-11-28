28dbdbf4b3af2ec706eeed7d0fe1362c67ff72fd
sandun7787 <sandunwarnasooreya345@gmail.com>
2024-11-28 14:19:32 +0530

    update

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import 'typeface-roboto';
+import App from './Containers/App';
+import registerServiceWorker from './registerServiceWorker';
+import {Provider} from 'react-redux';
+import {createStore,applyMiddleware} from 'redux';
+import reducer from './store/reducers/reducer';
+import thunk from 'redux-thunk';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const logger = store =>{
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    return next => {
+        return action => {
+            const result=next(action);
+            return result;
+        }
+    };
+}
+
+const store = createStore(reducer,applyMiddleware(logger,thunk));
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+registerServiceWorker();
