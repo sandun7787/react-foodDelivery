8ddde1aba016b2bfda1b4336c628ee1d43a76f07
sandun7787 <sandunwarnasooreya345@gmail.com>
2024-11-29 10:36:31 +0530

    update-appjs

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h2>Welcome to React</h2>
+      </header>
+      <main className="App-content">
+        <p>
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-      </div>
-    );
-  }
-}
+      </main>
+    </div>
+  );
+};
 
 export default App;
